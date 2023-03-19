a3fc00d7315c65aabe8f815224f9791f3e01d802
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-03-19 08:54:44 +0530

    laravel api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
+
+
+
+Route::post('/auth/register', [App\Http\Controllers\Api\AuthController::class, 'createUser']);
+Route::post('/auth/login', [App\Http\Controllers\Api\AuthController::class, 'loginUser']);
+
+
